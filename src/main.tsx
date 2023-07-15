37d2846b8abe15590ff3bc557cfb9ab1507ef3b8
Nano-AI <27119566+Nano-AI@users.noreply.github.com>
2023-07-14 23:43:39 -0700

    testing

--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import NavBar from './NavBar.tsx'
-import { Router, Route, createHashRouter, Routes, BrowserRouter } from 'react-router-dom';
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
 import About from './About.tsx';
 import Home from './Home.tsx';
 import './index.scss'
 import Footer from './Footer.tsx';
 // import "bootstrap/scss/bootstrap.scss";
 
-const router = createHashRouter([
-  {
-    path: "/",
-    element: <Home />
-  },
-  {
-    path: "/about",
-    element: <About />
-  }
-]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
