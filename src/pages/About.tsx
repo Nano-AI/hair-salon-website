250239cd5dbacdd85c9c1814a7b966f5e63ac9aa
Nano-AI <27119566+Nano-AI@users.noreply.github.com>
2023-07-26 21:45:38 -0700

    Added parallax scrolling to services page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,23 +1,31 @@
 import { Container } from "@mui/material";
-import { Row } from "react-bootstrap";
+import { Row, Carousel, CarouselItem } from "react-bootstrap";
+
+import Image1 from "../assets/about.jpg";
+import Image2 from "../assets/styling.jpg";
 
 export default function About() {
 
   return (
-    <div className="vh-100 w-100">
-      <Container className="h-100 w-100">
-        <Row className="h-100 w-100">
-            <div className="col-md-5 col-sm-12 m-5">
-              <br />
-              <br />
-              <h1 className="display-1">Who am I?</h1>
-              <p className="h3">Non occaecat officia velit esse ad exercitation quis dolore ex dolore. Pariatur voluptate sit excepteur id aute fugiat dolore sunt sunt et et duis. In consectetur occaecat consequat aute. In sunt dolor cupidatat consequat consequat mollit enim. Duis ut voluptate velit ipsum elit labore est anim. Laborum cillum deserunt esse laboris incididunt sit qui aliquip. Sint labore excepteur ad velit veniam nisi culpa.</p>
+    <div>
+      <div className="d-sm-block d-md-none" style={{ height: "6rem" }}></div>
+      <div className="vh-100 w-100 d-flex align-items-center justify-content-center">
+        <Container>
+          <Row>
+            <div className="col-md-6 col-sm-12 align-items-center justify-content-center d-inline-flex">
+              <Row>
+                <h1>About Me</h1>
+                <p className="h5">
+                  Veniam dolor ullamco irure aliqua labore cupidatat ullamco mollit fugiat occaecat commodo velit sunt. Nostrud amet non velit aliqua qui magna nulla Lorem deserunt excepteur. Proident officia ex labore et adipisicing consectetur consequat. Cupidatat cupidatat consequat exercitation nulla occaecat et. Sint officia in velit enim anim eiusmod id nulla dolore voluptate ex eu nostrud id. Officia reprehenderit non occaecat ipsum pariatur et anim aliqua commodo adipisicing laborum sunt tempor occaecat.
+                </p>
+              </Row>
             </div>
-            <div className="col-md-6 col-sm-12 h-100">
-              <div className="h-100 bg-black w-100"></div>
+            <div className="col-md-6 col-sm-12 align-items-center justify-content-center d-inline-flex">
+              <img src={Image1} className="img-thumbnail h-75" />
             </div>
-        </Row>
-      </Container>
+          </Row>
+        </Container>
+      </div>
     </div>
   );
 }
--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -1,6 +1,44 @@
+import { useRef } from "react";
+import { Parallax, ParallaxLayer } from "@react-spring/parallax";
+
+import Product from "../assets/product.jpg";
+import Room from "../assets/room.jpg";
+import Room2 from "../assets/room2.jpg";
+import Styling from "../assets/styling.jpg";
+
+
+
 export default function Services() {
   return (
-    <>
-    </>
+    <div className="bg-secondary">
+      <div style={{ height: "5rem" }}></div>
+      <div className="vh-100 vw-100">
+        <Parallax pages={2} className="h-100" id="testing">
+          <ParallaxLayer offset={0.1} speed={0.2} className="d-flex justify-content-center align-content-center">
+            <img src={Product} className="p-img rounded-5" />
+          </ParallaxLayer>
+          <ParallaxLayer offset={0.3} speed={0.6} className="d-flex justify-content-center align-content-center">
+            <span className="display-1 text-light overlay-text inverse-text">Hair Products</span>
+          </ParallaxLayer>
+          <ParallaxLayer offset={1} speed={0.2} className="d-flex justify-content-center align-content-center">
+            <img src={Styling} className="p-img rounded-5" />
+          </ParallaxLayer>
+          <ParallaxLayer offset={1.2} speed={0.6} className="d-flex justify-content-center align-content-center">
+            <span className="display-1 text-light overlay-text inverse-text">Hair Styling</span>
+          </ParallaxLayer>
+        </Parallax>
+      </div>
+      {/* {[
+        {
+          img: Product,
+          text: "Products"
+        }, 
+        {
+          img: Styling,
+          text: "Styling"
+        }
+      ].map((image) => (
+      ))} */}
+    </div>
   );
 }
